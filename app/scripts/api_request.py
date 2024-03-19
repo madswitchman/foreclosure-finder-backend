49a5948b9623402d5cd5241ae9cfb905e024a50e
madswitchman <madswitchman@yahoo.com>
2024-03-19 15:45:50 -0500

    Updating to utilize secret manager to set env vars

--- app/scripts/api_request.py
+++ app/scripts/api_request.py
@@ -56,7 +56,8 @@ def load_service_account_key():
                 "auth_uri": os.getenv('AUTH_URI'),
                 "token_uri": os.getenv('TOKEN_URI'),
                 "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
-                "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL')
+                "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL'),
+                "universe_domain": os.getenv('UNIVERSE_DOMAIN')
             }
         )
         return credentials
